81f862219ad46ea79af6859b9284b9939cb2ac0a
rsPogromishe <rs.pogromishe@gmail.com>
2022-03-30 20:51:54 +0400

    homework1 fixes done

--- Notes(Internship)/MainViewController.swift
+++ Notes(Internship)/MainViewController.swift
@@ -7,30 +7,34 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIApplicationDelegate {
-    
+class MainViewController: UIViewController {
     private var mainTextField = UITextView()
     private var titleTextField = UITextField()
     private var rightBarButton = UIBarButtonItem()
     private var mainContainer = UIView()
-    
+
     private var textIsEditing = true
+    private let keyForNote = "note"
+    private let keyForTitle = "title"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+
         navigationItem.title = "Заметки"
+
         setupRightBarButton()
         setupViewContainer()
         setupTitleTextField()
         setupMainTextView()
-    
-        NotificationCenter.default.addObserver(self,
-              selector: #selector(applicationWillTerminate(notification:)),
-              name: UIApplication.willTerminateNotification,
-              object: nil)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(applicationWillTerminate(notification:)),
+            name: UIApplication.willTerminateNotification,
+            object: nil
+        )
     }
-    
+
     @objc private func didRightBarButtonTapped(_ sender: Any) {
         textIsEditing.toggle()
         if textIsEditing {
@@ -44,63 +48,69 @@ class MainViewController: UIViewController, UITextFieldDelegate, UITextViewDeleg
             mainTextField.resignFirstResponder()
             titleTextField.isUserInteractionEnabled = false
             mainTextField.isEditable = false
+            UserDefaults.standard.set(mainTextField.text, forKey: keyForNote)
+            UserDefaults.standard.set(titleTextField.text, forKey: keyForTitle)
         }
     }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        UserDefaults.standard.set(titleTextField.text, forKey: "title")
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        UserDefaults.standard.set(mainTextField.text, forKey: "note")
-    }
-    
+
     @objc private func applicationWillTerminate(notification: Notification) {
         UserDefaults.standard.set(mainTextField.text, forKey: "note")
         UserDefaults.standard.set(titleTextField.text, forKey: "title")
     }
-    
+
     private func setupRightBarButton() {
         navigationItem.rightBarButtonItem = rightBarButton
         rightBarButton.title = "Готово"
         rightBarButton.target = self
         rightBarButton.action = #selector(didRightBarButtonTapped(_:))
     }
-    
+
     private func setupViewContainer() {
-        mainContainer.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(mainContainer)
-        mainContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        mainContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        mainContainer.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
-        mainContainer.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainContainer.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -20
+            ),
+            mainContainer.leftAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leftAnchor,
+                constant: 20
+            ),
+            mainContainer.rightAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.rightAnchor,
+                constant: -20
+            )
+        ])
     }
-    
+
     private func setupTitleTextField() {
-        titleTextField.translatesAutoresizingMaskIntoConstraints = false
         mainContainer.addSubview(titleTextField)
-        titleTextField.topAnchor.constraint(equalTo: mainContainer.topAnchor).isActive = true
-        titleTextField.leftAnchor.constraint(equalTo: mainContainer.leftAnchor, constant: 3.5).isActive = true
-        titleTextField.rightAnchor.constraint(equalTo: mainContainer.rightAnchor).isActive = true
-        titleTextField.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleTextField.topAnchor.constraint(equalTo: mainContainer.topAnchor),
+            titleTextField.leftAnchor.constraint(equalTo: mainContainer.leftAnchor, constant: 3.5),
+            titleTextField.rightAnchor.constraint(equalTo: mainContainer.rightAnchor),
+            titleTextField.heightAnchor.constraint(equalToConstant: 40.0)
+        ])
         titleTextField.adjustsFontSizeToFitWidth = false
         titleTextField.font = .systemFont(ofSize: 22.0, weight: .bold)
         titleTextField.placeholder = "Заголовок страницы"
-        titleTextField.delegate = self
-        titleTextField.text = UserDefaults.standard.string(forKey: "title") ?? ""
+        titleTextField.text = UserDefaults.standard.string(forKey: keyForTitle) ?? ""
     }
-    
+
     private func setupMainTextView() {
-        mainTextField.translatesAutoresizingMaskIntoConstraints = false
         mainContainer.addSubview(mainTextField)
-        mainTextField.leftAnchor.constraint(equalTo: mainContainer.leftAnchor).isActive = true
-        mainTextField.rightAnchor.constraint(equalTo: mainContainer.rightAnchor).isActive = true
-        mainTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor).isActive = true
-        mainTextField.bottomAnchor.constraint(equalTo: mainContainer.bottomAnchor).isActive = true
+        mainTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainTextField.leftAnchor.constraint(equalTo: mainContainer.leftAnchor),
+            mainTextField.rightAnchor.constraint(equalTo: mainContainer.rightAnchor),
+            mainTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor),
+            mainTextField.bottomAnchor.constraint(equalTo: mainContainer.bottomAnchor)
+        ])
         mainTextField.font = .systemFont(ofSize: 14.0, weight: .regular)
         mainTextField.becomeFirstResponder()
-        mainTextField.delegate = self
-        mainTextField.text = UserDefaults.standard.string(forKey: "note") ?? ""
+        mainTextField.text = UserDefaults.standard.string(forKey: keyForNote) ?? ""
     }
 }
-
